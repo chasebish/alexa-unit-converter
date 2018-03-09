7d8bbed3d8136c6a8e0de51a2efa5217906be6d1
chasebish <bishop.c@husky.neu.edu>
2018-03-08 17:06:37 -0800

    updated model

--- app/handlers/states/lengthState.js
+++ app/handlers/states/lengthState.js
@@ -1,4 +1,5 @@
-let outputSpeech, repromptSpeech
+
+let outputSpeech, repromptSpeech = 'Would you like to do any other length conversions?'
 
 const lengthState = {
 
@@ -9,7 +10,103 @@ const lengthState = {
 
         app.setFollowUpState('LengthState').ask(outputSpeech, repromptSpeech)
 
-    }
+    },
+
+    // =================================================================================    
+    // Inch Configuration
+    // =================================================================================
+
+    inchFoot: function (app) {
+
+    },
+    inchYard: function (app) {
+
+    },
+    inchMile: function (app) {
+
+    },
+    inchNanometer: function (app) {
+
+    },
+    inchMicrometer: function (app) {
+
+    },
+    inchMillimeter: function (app) {
+
+    },
+    inchCentimeter: function (app) {
+
+    },
+    inchMeter: function (app) {
+
+    },
+    inchKilometer: function (app) {
+
+    },
+
+    // =================================================================================    
+    // Foot Configuration
+    // =================================================================================
+
+    footInch: function (app) {
+
+    },
+    footYard: function (app) {
+
+    },
+    footMile: function (app) {
+
+    },
+    footNanometer: function (app) {
+
+    },
+    footMicrometer: function (app) {
+
+    },
+    footMillimeter: function (app) {
+
+    },
+    footCentimeter: function (app) {
+
+    },
+    footMeter: function (app) {
+
+    },
+    footKilometer: function (app) {
+
+    },
+
+    // =================================================================================    
+    // Inch Configuration
+    // =================================================================================
+
+    yardInch: function (app) {
+
+    },
+    yardFoot: function (app) {
+
+    },
+    yardMile: function (app) {
+
+    },
+    yardNanometer: function (app) {
+
+    },
+    yardMicrometer: function (app) {
+
+    },
+    yardMillimeter: function (app) {
+
+    },
+    yardCentimeter: function (app) {
+
+    },
+    yardMeter: function (app) {
+
+    },
+    yardKilometer: function (app) {
+
+    },
 
 }
 
